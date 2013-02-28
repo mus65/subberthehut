655f090a6b27795613862bfc7d7f1c126308dd24
Marius T <mus.svz@gmail.com>
2013-02-28 15:30:54 +0100

    version 3

--- subberthehut.c
+++ subberthehut.c
@@ -14,7 +14,7 @@
 #include <zlib.h>
 
 #define NAME                   "subberthehut"
-#define VERSION                "2"
+#define VERSION                "3"
 
 #define XMLRPC_URL             "http://api.opensubtitles.org/xml-rpc"
 #define LOGIN_LANGCODE         "en"
